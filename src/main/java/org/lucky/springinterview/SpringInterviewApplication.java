6d2cd227faf5bc44e1fd1bb37466dca232f46c26
lucky <lkapse63@gmail.com>
2024-04-09 17:53:18 +0530

    Boot code added

---
+++ src/main/java/org/lucky/springinterview/SpringInterviewApplication.java
@@ -0,0 +1,13 @@
+package org.lucky.springinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lucky/springinterview/SpringInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lucky.springinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
